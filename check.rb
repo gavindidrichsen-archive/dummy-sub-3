69c4fbfff12e5e7f3c20095a25d9937ae2f9eced
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2022-01-27 15:55:55 +0000

    update

--- check.rb
+++ check.rb
@@ -1,21 +1,10 @@
 #!/usr/bin/env ruby
-# require 'pry-byebug'
 
-# output = `git fetch --all --prune 2>&1`
 output = `git fetch --all --prune --dry-run 2>&1`
-# matches = output.scan(/(?<branch>\w+)\s+(?=->)/)
-# matches.each do |branch|
-#     puts "puppet code deploy #{branch[0]} --wait"
-# end
 lines = output.split(/\n/)
 lines.each do |line|
-
-    # matches = line.scan(/^\s+(\*\s\[(?<action>[^]]*)\]|(?<difference>[\w.]*))\s+(?<branch>\w+)\s+->/)
-    matches = line.match(%r{(?<branch>\w+)\s+(?=->)})
-    # binding.pry
+    matches =  line.match(%r{^\s+(\*\s\[(?<action>[^\]]*)\]|(?<difference>[\w.]*))\s+(?<branch>\w+)\s+->})
     next if matches.nil? 
-    # matches =  line.match(/^\s+(\*\s\[(?<action>[^]]*)\]|(?<difference>[\w.]*))\s+(?<branch>\w+)\s+->/)
-    # puts matches[:branch]
-    puts matches[:branch]
+    puts "#{matches[:branch]} [#{matches[:action]}#{matches[:difference]}]"
 end
 
